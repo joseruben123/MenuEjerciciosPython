2e3450b794679084363a2d0e50716ba141212ac2
joseruben123 <86092840+joseruben123@users.noreply.github.com>
2022-09-05 18:39:00 -0500

    Update PruebaPaquete.py

--- PruebaPaquete.py
+++ PruebaPaquete.py
@@ -12,6 +12,8 @@ Lo que hace __init__.py es convertir un directorio en un modulo (paquete)
 que contiene otros modulos, y esto lo hace para poder importarlos.
 
 """
+# para la ejecucion de este codigo se creo un paquete en pycharm con el nombre paquete1 y ahi se creo un archivo .py con el nombre que indica
+# para poder subirlo aqui en GitHub quedo esto obsoleto porque faltaria la ruta que pediria el archivo al ser ejecutado
 
 from paquete1.funcionesNumericas import*   # Esta linea importa las funciones del archivo funcionesNumericas
 from paquete1.funcionesCadena import *    # Esta linea importa las funciones del archivo funcionesCadena
